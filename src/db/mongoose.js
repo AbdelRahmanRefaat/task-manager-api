b1589499175605c62118d7040e42d2405def990f
AbdElRahman Refaat <abdelrahmanrefaat.m@gmail.com>
2021-07-15 13:05:48 +0200

    Fixed mongoose connection URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const log = console.log
 const mongoose = require('mongoose')
 
 const databaseName = 'task-manager-api'
-const connectionURL = `${process.env.MONGODB_URL+databaseName}`
+const connectionURL = process.env.MONGODB_URL
 mongoose.connect(connectionURL, { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
